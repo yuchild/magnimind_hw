6391d77d5925fc287f61d4605a43f6b00b136f7b
yuchild <yuchild@gmail.com>
2024-11-15 13:56:10 -0800

    Updated 2024-11-15

--- src/sum_binary.py
+++ src/sum_binary.py
@@ -12,6 +12,16 @@ def addTree(root):
         return 0
     return (root.key + addTree(root.left) + addTree(root.right))
 
+def addTreeRight(root):
+    if (root == None):
+        return 0
+    return (addTree(root.right))
+
+def addTreeLeft(root):
+    if (root == None):
+        return 0
+    return (addTree(root.left))
+
 if __name__ == '__main__':
     root = newNode(1)
     root.left = newNode(2)
@@ -19,9 +29,13 @@ if __name__ == '__main__':
     root.left.left = newNode(4)
     root.left.right = newNode(5)
     root.right.left = newNode(6)
-    root.right.left = newNode(7)
-    root.right.left.right = newNode(8)
+    root.right.right = newNode(7)
+    root.right.right.right = newNode(8)
     
-    sum = addTree(root)
+    sum_all = addTree(root)
+    sum_right = addTreeRight(root) 
+    sum_left = addTreeLeft(root)
     
-    print(f'Suma of all nodes is: {sum}')
+    print(f'Suma of all nodes is: {sum_all}')
+    print(f'Suma of all right nodes is: {sum_right}')
+    print(f'Suma of all left nodes is: {sum_left}')
